341e71f9ba3928fc5ee206216442502227407d74
Anh Minh (Matthew Nguyen) <76109800+annminn104@users.noreply.github.com>
2024-01-12 15:46:40 +0700

    feat: update config

---
+++ packages/configs/mui/mui-theme.d.ts
@@ -0,0 +1,30 @@
+import {
+  AlignItems,
+  Display,
+  FlexDirection,
+  JustifyContent,
+  AdjustFlex,
+} from "@core/global-types";
+import { Color } from "@mui/material";
+
+declare module "@mui/material/styles/createPalette" {
+  interface PaletteOptions {
+    red: Color;
+    yellow: Color;
+  }
+}
+
+declare module "@mui/material/styles/createTheme" {
+  interface ThemeOptions {
+    functions: AdjustFlex;
+  }
+  interface Theme {
+    functions: AdjustFlex;
+  }
+}
+
+declare module "@mui/material" {
+  interface SvgIconProps {
+    linearColor?: string;
+  }
+}
---
+++ packages/core/global-types/src/styles.ts
@@ -0,0 +1,109 @@
+export type GlobalValues =
+  | "initial"
+  | "inherit"
+  | "unset"
+  | "revert"
+  | "revert-layer";
+
+export type FlexDirection = "row" | "column";
+
+type DisplayMultiKeywordSyntax =
+  | "block flow"
+  | "inline flow"
+  | "inline flow-root"
+  | "block flex"
+  | "inline flex"
+  | "block grid"
+  | "inline grid"
+  | "block flow-root";
+
+export type Display =
+  | "block"
+  | "inline"
+  | "inline-block"
+  | "flex"
+  | "inline-flex"
+  | "grid"
+  | "inline-grid"
+  | "flow-root"
+  | "none"
+  | "contents"
+  | "table"
+  | "table-row"
+  | "list-item"
+  | DisplayMultiKeywordSyntax
+  | GlobalValues;
+
+type JustifyContentPositional =
+  | "center"
+  | "start"
+  | "end"
+  | "flex-start"
+  | "flex-end"
+  | "left"
+  | "right";
+
+export type JustifyContent =
+  | "normal"
+  | "space-between"
+  | "space-around"
+  | "space-evenly"
+  | "stretch"
+  | "safe center"
+  | "unsafe center"
+  | JustifyContentPositional
+  | GlobalValues;
+
+export type AlignItemsPositional =
+  | "center"
+  | "start"
+  | "end"
+  | "flex-start"
+  | "flex-end"
+  | "self-start"
+  | "self-end";
+
+export type AlignItemsBaseline =
+  | "baseline"
+  | "first baseline"
+  | "last baseline"
+  | "safe center"
+  | "unsafe center";
+
+export type AlignItems =
+  | "normal"
+  | "stretch"
+  | AlignItemsPositional
+  | AlignItemsBaseline
+  | GlobalValues;
+
+export type FontWeightRelative = "lighter" | "bolder";
+
+export type FontWeight =
+  | "normal"
+  | "bold"
+  | 100
+  | 200
+  | 300
+  | 400
+  | 600
+  | 700
+  | 800
+  | 900
+  | FontWeightRelative
+  | GlobalValues;
+
+export type AdjustFlex = {
+  pxToRem: (pixel: number, fontSize?: number) => string;
+  adjustFlex: (
+    display?: Display,
+    flexDirection?: FlexDirection,
+    justifyContent?: JustifyContent,
+    alignItems?: AlignItems
+  ) => {
+    display: Display;
+    flexDirection: FlexDirection;
+    justifyContent: JustifyContent;
+    alignItems: AlignItems;
+  };
+};
