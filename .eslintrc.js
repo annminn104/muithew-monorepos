a14d5a7e2b95bff8f3d05cfeb4edc3f663abae6b
Anh Minh (Matthew Nguyen) <76109800+annminn104@users.noreply.github.com>
2024-01-17 16:18:47 +0700

    fix: end of line crlf

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,23 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-refresh/only-export-components': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: true,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'crlf',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 150,
+        jsxSingleQuote: true
+      }
+    ]
   }
 };
--- apps/landing/app/provider.tsx
+++ apps/landing/app/provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React from 'react';
 import { MuiProvider } from '@core/providers';
+import React from 'react';
 
 interface RootProviderProps {
   children?: React.ReactNode;
