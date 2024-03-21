d02bafbfa02ab31f358306b11f9700d10b2c913a
Anh Minh (Matthew Nguyen) <76109800+annminn104@users.noreply.github.com>
2024-03-21 13:42:35 +0700

    fix: clean code

--- apps/maruhachi/utils/animate.ts
+++ apps/maruhachi/utils/animate.ts
@@ -1,16 +1,10 @@
 export class AnimateUtils {
   static whileInViewport = () => {
-    return {
-      viewport: { once: false, amount: 0.8 }
-    };
+    return { viewport: { once: false, amount: 0.8 } };
   };
-  static fadeIn = (duration = 0.5, delay = 0) => {
-    return {
-      ...this.whileInViewport,
-      initial: { opacity: 0, scale: 0.5 },
-      whileInView: { opacity: 1, scale: 1 },
-      transition: { duration, delay }
-    };
+
+  static fadeIn = (duration = 0.5, delay = 0: { duration: number; delay: number }) => {
+    return { ...this.whileInViewport, initial: { opacity: 0, scale: 0.5 }, whileInView: { opacity: 1, scale: 1 }, transition: { duration, delay } };
   };
 
   static fadeDirection = (direction = 'left', duration = 0.5, xDirection = 100, delay = 0) => {
