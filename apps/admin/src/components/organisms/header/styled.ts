7759aede59d70672e68d496562e207907e5f7e08
minhmatthewsb <minh.nca@sanbul.net>
2024-04-12 14:53:49 +0700

    feat: update header admin

--- apps/admin/src/components/organisms/header/styled.ts
+++ apps/admin/src/components/organisms/header/styled.ts
@@ -1,4 +1,5 @@
-import { styled } from '@mui/material';
+import { styled, TextField } from '@mui/material';
+import { SearchIcon } from '@styles/ui-mui/icons';
 
 export const Header = styled(
   'div',
@@ -7,3 +8,17 @@ export const Header = styled(
   ...theme.functions.adjustFlex('flex', 'row', 'flex-start', 'center'),
   columnGap: '16px'
 }));
+
+export const HeaderLeft = styled(
+  'div',
+  {}
+)(({ theme }) => ({
+  ...theme.functions.adjustFlex('flex', 'row', 'flex-start', 'center'),
+  columnGap: '8px'
+}));
+
+export const HeaderLeftInputSearch = styled(TextField, {})(() => ({ svg: { opacity: 0.6 } }));
+
+export const HeaderLeftIcon = styled(SearchIcon, {})(() => ({}));
+
+export const HeaderRight = styled('div', {})(() => ({}));
