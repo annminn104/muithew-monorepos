d4ace1e97d6f28fe6d589e18d15c0118d0878c50
minhmatthewsb <minh.nca@sanbul.net>
2024-05-31 15:05:10 +0700

    fix: fix clerk saas builder env pre-build

--- apps/saas-builder/app/provider.tsx
+++ apps/saas-builder/app/provider.tsx
@@ -3,17 +3,24 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
 import { MuiReactProvider } from '@core/providers';
+import { isEmpty } from 'lodash';
 import React from 'react';
 interface RootProviderProps {
   children?: React.ReactNode;
 }
 
 const RootProvider: React.FC<RootProviderProps> = ({ children }) => {
-  return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
-      <MuiReactProvider defaultMode='dark'>{children}</MuiReactProvider>
-    </ClerkProvider>
-  );
+  return hasClerkProvider(<MuiReactProvider defaultMode='dark'>{children}</MuiReactProvider>);
 };
 
 export default RootProvider;
+
+const hasClerkProvider = (children?: React.ReactNode): React.JSX.Element => {
+  return !isEmpty(process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY) ? (
+    <ClerkProvider appearance={{ baseTheme: dark }} publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      {children}
+    </ClerkProvider>
+  ) : (
+    <React.Fragment>{children}</React.Fragment>
+  );
+};
