820a659101d4e3a47a4d4c1cfd181cdcc6ee9e14
Matthew Nguyen <annminn104@gmail.com>
2024-10-06 21:42:42 +0700

    feat(portfolio): update cv

--- apps/portfolio/common/mocks/info.tsx
+++ apps/portfolio/common/mocks/info.tsx
@@ -7,7 +7,7 @@ export const InfoMocks = {
   summary: [
     'With over 3 years of experiences as a Frontend Developer, I specialize in creating dynamic and responsive web applications using Angular, ReactJS, and NextJS. My expertise includes developing robust user interfaces, implementing state management, and ensuring seamless performance across various devices. I am proficient in HTML, CSS, JavaScript, and have a strong background in TypeScript and modern Frontend libraries such as Redux and RxJS. I have a proven track record of delivering high-quality, maintainable code, collaborating effectively with cross-functional teams, and continuously learning and adapting to new technologies.'
   ],
-  resume: 'https://drive.google.com/file/d/1rXEa4Uwye0iTvup6qkqUTDUJkf1tTeBH/view?usp=sharing',
+  resume: 'https://drive.google.com/file/d/1pW576rhzfD5wVSj_PQmfSFsYPHUd3NuA/view?usp=sharing',
   socials: [
     {
       name: 'linkedin',
@@ -40,6 +40,8 @@ export const InfoMocks = {
         'Ant Design',
         'Ng-Zorro',
         'Angular Material',
+        'Shacdn/ui',
+        'Radix UI',
         'MUI React',
         'Framer Motion',
         'GSAP',
@@ -48,7 +50,7 @@ export const InfoMocks = {
     },
     {
       title: 'HeadlessCMS',
-      keys: ['Hygraph', 'Strapi']
+      keys: ['Hygraph', 'Strapi', 'Directus', 'Payload CMS', 'StoryBlok']
     },
     {
       title: 'Testing tools',
@@ -79,10 +81,38 @@ export const InfoMocks = {
   ],
   experiences: [
     {
+      company: ['Gearment'],
+      position: ['Frontend Developer'],
+      startDate: '08/2024',
+      endDate: 'Present',
+      responsibilities: [
+        'Maintained a landing website using Remix and Directus CMS (headless CMS).',
+        'Optimized website performance and SEO for improved user experience and search engine rankings.',
+        'Refactored and structured code to ensure reusable components, enhancing maintainability and scalability.',
+        'Integrated APIs using connectrpc for seamless communication between frontend and backend services.',
+        'Implemented authentication with Facebook and Google login for user convenience.',
+        'Contributed to key modules including orders, stores, studios, members, and logs, ensuring smooth functionality and efficient workflow for the admin system.'
+      ],
+      techStacks: [
+        {
+          title: 'Programming languages:',
+          keys: 'ReactJS 18, Remix, HTML, SCSS, Javascript, Typescript.'
+        },
+        {
+          title: 'UI libraries:',
+          keys: 'Shacdn/ui, Radix UI, TailwindCSS'
+        },
+        {
+          title: 'Other:',
+          keys: 'CMS Directus, Turbo, Zustand, React-Query, ConnectRPC, Github Actions, Framer Motion,...'
+        }
+      ]
+    },
+    {
       company: ['Sanbul Solutions'],
       position: ['Frontend Developer'],
-      startDate: '15/04/2023',
-      endDate: 'Present',
+      startDate: '04/2023',
+      endDate: '8/2024',
       responsibilities: [
         'Create and assign tasks using Jira.',
         'Develop the source code structure following Atomic-Design principles.',
